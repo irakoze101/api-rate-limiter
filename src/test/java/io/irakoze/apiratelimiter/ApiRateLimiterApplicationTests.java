866acc5d1b80962f4da81cc9ef9f452a9cdf3c4a
ivan.irakoze <ib.irakoze@gmail.com>
2023-03-10 10:41:22 +0200

    First commit

---
+++ src/test/java/io/irakoze/apiratelimiter/ApiRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.irakoze.apiratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRateLimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
